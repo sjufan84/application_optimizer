2e19c19326bc4c41901883ce2ed683de5560bbeb
David Thomas <dave_thomas@enoughwebapp.com>
2023-11-20 16:56:48 -0600

    updated UI

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """ PR Prophet -- The one and only 
 by: @ChickenMon84 """
 import os
-import asyncio
 from dotenv import load_dotenv
 import streamlit as st
 from openai import OpenAI
-from PIL import Image
+from utils.chat_utils import create_thread_run, add_message_and_run
+from utils.assistant_utils import list_assistant_files, delete_assistant_files, upload_files, create_assistant_files
 
 # Load environment variables
 load_dotenv()
@@ -21,16 +21,121 @@ client = OpenAI(api_key=api_key, organization=organization, timeout=10, max_retr
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4-1106-preview"
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+def process_uploaded_files(uploaded_files):
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            with st.form(key=uploaded_file.name):
+                st.write(f"Metadata for {uploaded_file.name}")
+                file_category = st.selectbox("File category:", ["Resume", "Job Description"], key=f"category_{uploaded_file.name}")
+                file_description = st.text_area("Short description (optional):", key=f"desc_{uploaded_file.name}")
+                submit_metadata = st.form_submit_button("Submit Metadata")
+                file_contents = [file.read() for file in uploaded_files]
+
+                if submit_metadata:
+                    handle_file_upload(file_contents, file_category, file_description)
+
+def handle_file_upload(file_contents, category, description):
+    file_content = [content for content in file_contents]
+    response = upload_files(files=file_content)
+    file_ids = response
+    
+    if "file_metadata" not in st.session_state:
+        st.session_state.file_metadata = {}
+    st.session_state.file_metadata[response[0]] = {"category": category, "description": description}
+    
+    create_assistant_file(file_ids)
+
+def create_assistant_file(file_ids):
+  for file_id in file_ids:
+    assistant_file = client.beta.assistants.files.create(assistant_id=st.session_state.assistant_id, file_id=file_id)
+    st.session_state.assistant_files.append(assistant_file)
+
+def process_pasted_text(text):
+    if text:
+        # Here you can add logic to process or store the pasted text
+        if "pasted_texts" not in st.session_state:
+            st.session_state.pasted_texts = []
+        st.session_state.pasted_texts.append(text)
+        # Convert the text to bytes
+        text_bytes = text.encode("utf-8")
+        # Upload the file contents and the metadata to the assistant
+        file_ids = upload_files([text_bytes])
+        # Assistant files creation
+        assistant_files = create_assistant_files(file_ids=file_ids)
+        # Append the assistant files to the session state
+        st.session_state.assistant_files = st.session_state.assistant_files + assistant_files
+
+        return file_ids 
+
+def process_user_input(input_text):
+    if input_text:
+        st.session_state.messages.append({"role": "user", "content": input_text})
+        response = create_thread_or_add_message(input_text)
+        st.session_state.messages.append({"role": "assistant", "content": response["message"]})
+
+def create_thread_or_add_message(input_text):
+    thread_id = st.session_state.get("thread_id")
+    file_ids = st.session_state.get("file_ids")
+
+    if thread_id is None:
+        response = create_thread_run(message_content=input_text, file_ids=file_ids)
+        st.session_state.thread_id = response["thread_id"]
+    else:
+        response = add_message_and_run(message_content=input_text, thread_id=thread_id, file_ids=file_ids)
+
+    return response
 
 def main():
     """ Main function for Application Optimizer """
-    st.markdown("""
-                <h3 style='text-align: center; color: #f6bd60;'>PR Prophet.
-                Welcome to the application optimizer</h3>
-                """, unsafe_allow_html=True)
-    st.text("")
+    st.title("Application Optimizer")
+
+    # Initialize session state variables if they don't exist
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    if "file_ids" not in st.session_state:  
+        st.session_state.file_ids = []
+    if "assistant_files" not in st.session_state:
+        st.session_state.assistant_files = []
+    if "thread_id" not in st.session_state:
+        st.session_state.thread_id = None
+    if "assistant_id" not in st.session_state:
+        st.session_state.assistant_id = "asst_FNbc0H90UCKiCu7DR41Ot3Mp"
+
+    # Sidebar for file uploads and text input
+    with st.sidebar:
+        st.header("Upload Files and Text")
+        uploaded_files = st.file_uploader("Upload your files here:", accept_multiple_files=True)
+        process_uploaded_files(uploaded_files)
+
+        pasted_text = st.text_area("Or paste text here:", height=200)
+        if st.button("Submit Text"):
+            process_pasted_text(pasted_text)
+        
+    # Chat input
+    user_input = st.text_input("Type your message here...")
+    if st.button("Send"):
+        process_user_input(user_input)
+        display_chat_interface()
+
+def display_example_prompts():
+    """ Display example prompts to guide the user """
+    st.markdown("##### To get started, begin your chat below. You can simply start a chat with a question or at any time upload any relevant files that you want to be considered in the optimization process.")
+    # List some example prompts
+    st.markdown("###### Example prompts:")
+    st.markdown("**1. What are the top 3 things I should do to optimize my application?**")
+    st.markdown("**2. What keywords from the job description should I include in my application?**")
+    st.markdown("**3. What areas can I help the applicant improve in their application?**")
+    st.markdown("**4. From 1 to 10, how would you rate the applicant's fit for the job?**")
+
+def display_chat_interface():
+    # Clear existing chat display
+    st.empty()
+
+    # Display example prompts only if there are no messages
+    if not st.session_state.messages:
+        display_example_prompts()
+
+    # Display chat messages
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         if message["role"] == "assistant":
@@ -39,29 +144,11 @@ def main():
         elif message["role"] == "user":
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
+        
 
-    # Accept user input
-    if prompt := st.chat_input("Let's get started!"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        # Load the prophet image for the avatar
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
 
-        response = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages= [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-            stream=True,
-            temperature=1,
-            max_tokens=100
-            ):
-            FULL_RESPONSE += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(FULL_RESPONSE + "▌")
-        message_placeholder.markdown(FULL_RESPONSE)
-        st.session_state.messages.append({"role": "assistant", "content": FULL_RESPONSE})
 if __name__ == "__main__":
-    asyncio.run(stream_pr())
+    main()
+    st.write(st.session_state.messages)
+    st.write(list_assistant_files(st.session_state.assistant_id))
+    
--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 from openai import OpenAI
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.getcwd()))))
 
-from app.models.runs import CreateThreadRunRequest, CreateMessageRunRequest  # noqa: E402
 from utils.assistant_utils import upload_files  # noqa: E402
 from utils.run_utils import poll_run_status  # noqa: E402
 
@@ -22,16 +21,19 @@ OPENAI_ORG_ID = os.getenv("OPENAI_ORG2")
 # Set up the client
 client = OpenAI(api_key=OPENAI_API_KEY, organization=OPENAI_ORG_ID, max_retries=3, timeout=10)
 
+assistant_id = "asst_FNbc0H90UCKiCu7DR41Ot3Mp"
+
 # Define the function to create a thread and run
-async def create_thread_run(create_run_request: CreateThreadRunRequest, thread_id: str=None):
+def create_thread_run(message_content: str, thread_id: str=None, file_ids: List[str]=None):
   """ Create a thread and run """
   run = client.beta.threads.create_and_run(
-  assistant_id=create_run_request.assistant_id,
+  assistant_id=assistant_id,
   thread={
     "messages": [
         {
           "role" : "user",
-          "content" : create_run_request.message_content, 
+          "content" : message_content, 
+          "file_ids" : file_ids if file_ids else []
     }]}   
   )
   # Poll the run status
@@ -39,29 +41,29 @@ async def create_thread_run(create_run_request: CreateThreadRunRequest, thread_i
 
   return response
   
-async def add_message_and_run(message_request: CreateMessageRunRequest):
+def add_message_and_run(message_content: str, thread_id: str=None, file_ids: List[str]=None):
     """ Add a message to the thread and run """
-    
     # Create and send the message
     message = client.beta.threads.messages.create(
-        message_request.thread_id,
-        content=message_request.message_content,
+        thread_id = thread_id,
+        content=message_content,
         role="user",
+        file_ids=file_ids if file_ids else [],  
     )
     # Log the message
     logging.info(f"Message created: {message}")
 
     # Create the run
     run = client.beta.threads.runs.create(
-        assistant_id=message_request.assistant_id,
-        thread_id=message_request.thread_id
+        assistant_id=assistant_id,
+        thread_id=thread_id,
     )
     # Poll the run status
     response = poll_run_status(run_id=run.id, thread_id=run.thread_id)
 
     return response
 
-async def extract_text_from_image(files: List[UploadFile] = File(None)):
+def extract_text_from_image(files: List[UploadFile] = File(None)):
     """ Extract text from an image """
     # If there are uploaded files, pass them to the upload_files function
     #if files:
@@ -73,8 +75,9 @@ async def extract_text_from_image(files: List[UploadFile] = File(None)):
 
     #return formatted_text
 
-async def upload_assistant_files(files: List[UploadFile] = File(None)):
+def upload_assistant_files(files: List[UploadFile] = File(None)):
   """ Upload files to OpenAI and return the file IDs """
-  file_contents = [await file.read() for file in files]
-  file_ids = await upload_files(file_contents)
+  file_contents = [file.read() for file in files]
+  file_ids = upload_files(file_contents)
   return file_ids
+
